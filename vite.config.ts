62f6e93f55534bfe2666c12ecf17140c49237b8c
Antonio González <antonio.gonzalez@codeoscopic.com>
2023-04-27 19:39:33 +0200

    chore: update ts and vite config

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,12 @@ const rootPath = path.resolve(__dirname, "src");
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,
+  },
+  preview: {
+    port: 8080,
+  },
   resolve: {
     alias: [{ find: "@", replacement: rootPath }],
   },
@@ -22,9 +28,11 @@ export default defineConfig({
   },
   test: {
     globals: true,
+    exclude: ["node_modules/", "src/test/setupTests.ts"],
     environment: "jsdom",
     setupFiles: `${rootPath}/test/setupTest.ts`,
     css: true,
-    testTimeout: 60000,
+    testTimeout: 30000,
+    hookTimeout: 30000,
   },
 });
